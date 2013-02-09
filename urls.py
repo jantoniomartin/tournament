42e7cb10f5aed189dd613fa74fc17d034fcb2478
Jose Antonio Martin Prieto <jantonio.martin@gmail.com>
2013-02-09 12:54:10 +0100

    add views for tournament games

--- urls.py
+++ urls.py
@@ -6,4 +6,6 @@ urlpatterns = patterns('tournament.views',
 	url(r'^$', views.LatestTournamentDetailView.as_view(), name="latest-tournament"), 
 	url(r'^(?P<slug>\w+)$', views.TournamentDetailView.as_view(), name="tournament-detail"),
 	url(r'^register/(?P<slug>\w+)$', views.RegisterParticipantView.as_view(), name="register-participant"),
+	url(r'^active/(?P<slug>\w+)$', views.ActiveGamesListView.as_view(), name="tournament-active-games"),
+	url(r'^finished/(?P<slug>\w+)$', views.FinishedGamesListView.as_view(), name="tournament-finished-games"),
 )
--- views.py
+++ views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.decorators import login_required
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from django.shortcuts import get_object_or_404
+from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic.base import View, TemplateView
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from django.utils.decorators import method_decorator
 
 import tournament.models as tournament
@@ -17,6 +19,18 @@ class LatestTournamentDetailView(TournamentDetailView):
 
 	def get_object(self):
 		return tournament.Tournament.objects.latest('created_on')
+	
+	def get_context_data(self, **kwargs):
+		context = super(LatestTournamentDetailView, self).get_context_data(**kwargs)
+		t = self.get_object()
+		try:
+			current_stage = tournament.Stage.objects.get(tournament=t, started_on__isnull=False,
+				finished_on__isnull=True)
+		except ObjectDoesNotExist, MultipleObjectsReturned:
+			current_stage = tournament.Stage.objects.none()
+		context.update({"current_stage": current_stage,})
+		
+		return context
 
 class RegisterParticipantView(TemplateView):
 	template_name = "tournament/register.html"
@@ -39,3 +53,39 @@ class RegisterParticipantView(TemplateView):
 	@method_decorator(login_required)
 	def dispatch(self, *args, **kwargs):
 		return super(RegisterParticipantView, self).dispatch(*args, **kwargs)
+
+class ActiveGamesListView(ListView):
+	template_name = 'tournament/game_list_active.html'
+	model = tournament.TournamentGame
+	paginate_by = 10
+	context_object_name = 'game_list'
+	
+	def get_queryset(self):
+		t = get_object_or_404(tournament.Tournament, slug=self.kwargs['slug'])
+		return tournament.TournamentGame.objects.filter(stage__tournament=t,
+			started__isnull=False, finished__isnull=True)
+	
+	def get_context_data(self, **kwargs):
+		context = super(ActiveGamesListView, self).get_context_data(**kwargs)
+		t = get_object_or_404(tournament.Tournament, slug=self.kwargs['slug'])
+		context.update({"tournament": t})
+		return context
+
+class FinishedGamesListView(ListView):
+	template_name = 'tournament/game_list_finished.html'
+	model = tournament.TournamentGame
+	paginate_by = 10
+	context_object_name = 'game_list'
+	
+	def get_queryset(self):
+		t = get_object_or_404(tournament.Tournament, slug=self.kwargs['slug'])
+		return tournament.TournamentGame.objects.filter(stage__tournament=t,
+			started__isnull=False, finished__isnull=False)
+	
+	def get_context_data(self, **kwargs):
+		context = super(FinishedGamesListView, self).get_context_data(**kwargs)
+		t = get_object_or_404(tournament.Tournament, slug=self.kwargs['slug'])
+		context.update({"tournament": t})
+		return context
+
+
