295b1b7ae93c6f0f490d76887e2b69d55a070d5d
José Antonio Martín Prieto <jantonio.martin@gmail.com>
2019-09-08 23:41:39 +0200

    removed django.conf.urls.patterns

--- urls.py
+++ urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import *
+from django.conf.urls import url
 
 import tournament.views as views
 
-urlpatterns = patterns('tournament.views',
+urlpatterns = [
 	url(r'^$', views.LatestTournamentDetailView.as_view(), name="latest-tournament"), 
 	url(r'^(?P<slug>\w+)$', views.TournamentDetailView.as_view(), name="tournament-detail"),
 	url(r'^register/(?P<slug>\w+)$', views.RegisterParticipantView.as_view(), name="register-participant"),
 	url(r'^active/(?P<slug>\w+)$', views.ActiveGamesListView.as_view(), name="tournament-active-games"),
 	url(r'^finished/(?P<slug>\w+)$', views.FinishedGamesListView.as_view(), name="tournament-finished-games"),
-)
+]
